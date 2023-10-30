308f428d7ffe604ac2762cceedfc90edc89ba21b
Rayfa79 <96704831+Rayfa79@users.noreply.github.com>
2023-10-29 22:15:05 -0400

    Added Routing
    Added Routing to Forms app

--- template-driven-form/src/app/app-routing.module.ts
+++ template-driven-form/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingFormComponent } from './shopping-form/shopping-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'shopping-form', component: ShoppingFormComponent},
+  {path: 'template-form', component: TemplateFormComponent},
+  {path: "", redirectTo: 'shopping-form', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
